680c10646cc5de1f83226369ba7bc643f79005db
John valenzuela <johnhvalenzuela@gmail.com>
2023-06-03 14:28:51 -0400

    solutions

--- practice-for-week-05-array-long-practice-main/array-practice.js
+++ practice-for-week-05-array-long-practice-main/array-practice.js
@@ -1,23 +1,61 @@
 const findMinimum = arr => {
+  // O(n) Time
+  // O(1) Space
+  let  min  = arr[0];
 
-  // Your code here
-
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  return min;
 };
 
 const runningSum = arr => {
+  // O(n) Time
+  // O(1) Space
+  const result = [];
+  let sum = 0;
 
-  // Your code here
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    result.push(sum);
+  }
+
+  return result;
 };
 
 const evenNumOfChars = arr => {
+  // O(1) Time
+  // O(1) Space
+  let count = 0;
 
-  // Your code here
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length % 2 === 0) {
+      count++;
+    }
+  }
+
+  return count;
 };
 
 const smallerThanCurr = arr => {
+  // O(n^2) Time
+  // O(n) Space
+  const result = [];
 
-  // Your code here
+  for(let i = 0; i < arr.length; i++) {
+    let count = 0;
 
+    for(let j = 0; j < arr.length; j++) {
+      if(arr[j] < arr[i]) {
+        count++;
+      }
+    }
+
+    result.push(count);
+  }
+  return result;
 };
 
 const twoSum = (arr, target) => {
